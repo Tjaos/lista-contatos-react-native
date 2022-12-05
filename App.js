ce6db129904fc14e418b5d03569cb569433d45c1
tjaos <Thiago>
2022-12-05 01:41:13 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useTransition } from "react";
+import { Button, TextInput, StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function App() {
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: "column",
+        },
+      ]}
+    >
+      <View style={styles.containerLogin}>
+        <Icon style={styles.icon} name="user" size={100} color="#000" />
+        <Text style={styles.login}> login</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.senha}> senha</Text>
+        <TextInput style={styles.input} secureTextEntry={true} />
+      </View>
+
+      <View style={styles.botaoLogin}>
+        <Button style={styles.botaoLogin} title="Login" />
+      </View>
+
+      <View style={styles.botaoCadastro}>
+        <Button style={styles.botaoCadastro} title="cadastre-se" />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+    alignItems: "center",
+  },
+  containerLogin: {
+    top: "15%",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  input: {
+    height: 50,
+    width: "85%",
+    backgroundColor: "#fff",
+    top: "32%",
+  },
+  icon: {
+    top: "20%",
+  },
+  login: {
+    top: "30%",
+    right: "30%",
+    fontSize: 30,
+  },
+  senha: {
+    top: "30%",
+    right: "30%",
+    fontSize: 30,
+  },
+  botaoLogin: {
+    height: 50,
+    width: "85%",
+    top: "32%",
+  },
+  botaoCadastro: {
+    height: 50,
+    width: "85%",
+    top: "34%",
+    color: "red",
   },
 });
+
+export default Flex;
